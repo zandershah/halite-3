96da5bef11985fe8c1d4064cffc451b11fb76590
ZanderShah <alexander.shah@uwaterloo.ca>
2019-01-15 11:49:48 -0500

    smarter dropoff saving

--- run_game.py
+++ run_game.py
@@ -10,4 +10,4 @@ bots = ['./MyBot'] + ['./bots/Jan13'] * 3
 
 seed = randint(0, (1 << 31) - 1) if len(sys.argv) == 1 else sys.argv[1]
 
-os.system('./halite --replay-directory replays/ --width 48 --height 48 -vvv --no-logs --no-timeout --seed {} {}'.format(seed, ' '.join(bots)))
+os.system('./halite --replay-directory replays/ --width 40 --height 40 -vvv --no-logs --no-timeout --seed {} {}'.format(seed, ' '.join(bots)))
