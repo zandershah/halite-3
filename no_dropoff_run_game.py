e98ffefaf9e668ce77d07f8fe3f28516103ce58f
ZanderShah <alexander.shah@uwaterloo.ca>
2019-01-15 20:44:05 -0500

    u

--- no_dropoff_run_game.py
+++ no_dropoff_run_game.py
@@ -6,8 +6,7 @@ os.system('set -e')
 os.system('cmake .')
 os.system('make')
 
-bots = ['./MyBot'] + ['./bots/Jan13NoDropoff'] * 3
-bots = ['./MyBot'] + ['./Test'] * 3
+bots = ['./MyBot'] + ['./bots/Jan15NoDropoff'] * 3
 
 seed = randint(0, (1 << 31) - 1) if len(sys.argv) == 1 else sys.argv[1]
 
