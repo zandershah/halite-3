62bff7fcddc489ca73245deb0a00c2189b70658f
ZanderShah <alexander.shah@uwaterloo.ca>
2019-01-17 12:27:37 -0500

    try to plan smarter in 2p as well

--- no_dropoff_run_game.py
+++ no_dropoff_run_game.py
@@ -6,8 +6,8 @@ os.system('set -e')
 os.system('cmake .')
 os.system('make')
 
-bots = ['./MyBot'] + ['./bots/Jan15NoDropoff'] * 3
+bots = ['./MyBot'] + ['./bots/Jan15NoDropoff'] * 1
 
 seed = randint(0, (1 << 31) - 1) if len(sys.argv) == 1 else sys.argv[1]
 
-os.system('./halite --no-logs --replay-directory replays/ --no-timeout -vvv --seed {} {}'.format(seed, ' '.join(bots)))
+os.system('./halite --no-logs --replay-directory replays/ --width 32 --height 32 --no-timeout -vvv --seed {} {}'.format(seed, ' '.join(bots)))
